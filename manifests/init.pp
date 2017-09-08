8a05e2ce4e65e2cdac50ad53d6ce1dc5797df1e3
Romain Tartière <romain@blogreen.org>
2017-09-08 18:13:43 +0200

    (#50) Fix configuration file path
    On FreeBSD the extpected gnatsd configuration file is
    /usr/local/etc/gnatsd.conf, that is
      - /etc becomes /usr/local/etc;
      - there is no gnatsd subdirectory;
      - the config files extension is ".conf", and not ".cfg".

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,6 +14,7 @@ class nats (
   Boolean $announce_cluster = false,
   String $binpath = "/usr/sbin/gnatsd",
   String $configdir = "/etc/gnatsd",
+  String $configfile = "/etc/gnatsd/gnatsd.cfg",
   String $piddir = "/var/run",
   String $binary_source = "puppet:///modules/nats/gnatsd-1.0.0",
   String $service_name = "gnatsd",
