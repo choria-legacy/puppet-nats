b9149fd0046f47a2c0d6aecca893a92b7be08753
Andreas Ntaflos <daff@pseudoterminal.org>
2017-09-25 20:06:43 +0200

    (#57) Don't include class systemd in install/systemd.pp
    The `systemd` class is already included by `systemd::unit_file` so it is
    unnecessary here.
    
    This also prevents unwanted restarts of the `gnatsd` service that happen
    when other classes use defined types from the systemd module, for
    example when managing a drop-in file using `systemd::dropin_file`. Such
    defined types trigger `Exec[systemctl-daemon-reload]` which propagates
    through to the `nats::install::systemd` class which notifies the
    `nats::service` class. Not including the `systemd` class here fixes
    that.

--- manifests/install/systemd.pp
+++ manifests/install/systemd.pp
@@ -1,6 +1,5 @@
 # Manage systemd unit
 class nats::install::systemd {
-  contain "::systemd"
 
   systemd::unit_file { "${nats::service_name}.service":
     content => epp("nats/systemd_service.epp"),
